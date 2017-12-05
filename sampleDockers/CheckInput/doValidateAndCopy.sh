c017a6371a12cb755c39873b20270cbbd0fd85c6
José María Fernández <jose.m.fernandez@bsc.es>
2017-12-05 12:47:56 +0100

    Added the input checking Docker example

---
+++ sampleDockers/CheckInput/doValidateAndCopy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ $# = 2 ] ; then
+	input="$1"
+	output="$2"
+	if [ -f "$input" -a -r "$input" ] ; then
+		case "$(file "$input")" in
+			*\ gzip\ compressed*)
+				# As gzip is the canonical format for this
+				# example, we only have to copy it
+				if gunzip -t "$input" ; then
+					exec cp "$input" "$output"
+				else
+					echo "ERROR: Corrupted input (gzip) file!" 1>&2
+					exit 3
+				fi
+				;;
+			*\ bzip2\ compressed*)
+				# As gzip is the canonical format for this
+				# example, we have to translate the bzip2
+				# compressed file into a gzip one
+				if bunzip2 -t "$input" ; then
+					bunzip2 -c "$input" | gzip -9c > "$output"
+					exit $?
+				else
+					echo "ERROR: Corrupted input (bzip2) file!" 1>&2
+					exit 3
+				fi
+				;;
+			*)
+				echo "ERROR: Unrecognized format!" 1>&2
+				exit 2
+				;;
+		esac
+	else
+		echo "ERROR: input file is unreadable!" 1>&2
+		exit 1
+	fi
+else
+	echo "Usage: $0 {input} {output}" 1>&2
+	exit 1
+fi
