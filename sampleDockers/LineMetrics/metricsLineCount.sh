e52bd6901b41a6c73be9bfb67530437108636db2
José María Fernández <jose.m.fernandez@bsc.es>
2017-12-07 17:24:51 +0100

    Added line, word and word distribution sample metrics

---
+++ sampleDockers/LineMetrics/metricsLineCount.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ $# = 3 ] ; then
+	input="$1"
+	# This parameter is not being used in this example
+	reference="$2"
+	output="$3"
+	numlines=$(gunzip -c "$input" | wc -l)
+	if [ $? = 0 ] ; then
+		numwords=$(gunzip -c "$input" | wc -w)
+		if [ $? = 0 ] ; then
+			cat<<EOF > "$output"
+{
+	"id": "$(basename "$input")",
+	"metrics": [
+		{
+			"type": "linecount",
+			"value": $numlines,
+			"units": "lines"
+		},
+		{
+			"type": "wordcount",
+			"value": $numwords,
+			"units": "words"
+		}
+	]
+}
+EOF
+		else
+			retval=$?
+		fi
+	else
+		retval=$?
+	fi
+
+	if [ -n "$retval" ] ; then
+		echo "ERROR: Counting lines metrics computation failed" 1>&2
+		exit $retval
+	else
+		exit 0
+	fi
+else
+	echo "Usage: $0 {input} {reference} {output}" 1>&2
+	exit 1
+fi
---
+++ sampleDockers/WordMetrics/metricsWordDist.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# = 3 ] ; then
+	input="$1"
+	# This parameter is not being used in this example
+	reference="$2"
+	output="$3"
+
+	uniqtemp="$(mktemp -t uniqXXXXXX)"
+	gunzip -c "$input" | strings | tr -s ' ,:.' '\n' | sort | uniq -c | sed 's#^[^0-9]*\([0-9]\+\) \+\(.\+\)$#\t\t\t\t"\2": \1,#g;$ s#,$##;' > "$uniqtemp"
+	if [ $? = 0 ] ; then
+		cat<<EOF > "$output"
+{
+	"id": "$(basename "$input")",
+	"metrics": [
+		{
+			"type": "worddist",
+			"units": "worddist",
+			"value": {
+$(<"${uniqtemp}")
+			}
+		}
+	]
+}
+EOF
+	else
+		retval=$?
+	fi
+	rm -f "$uniqtemp"
+
+	if [ -n "$retval" ] ; then
+		echo "ERROR: Word distribution metrics computation failed" 1>&2
+		exit $retval
+	else
+		exit 0
+	fi
+else
+	echo "Usage: $0 {input} {reference} {output}" 1>&2
+	exit 1
+fi
