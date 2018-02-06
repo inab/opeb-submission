80b9e37a645adf2c85f218738a667642052b9393
Miguel Madrid Mencía <mimadrid@ucm.es>
2018-02-06 16:15:44 +0100

    VM: Updated tests path folder

--- scripts/tests/guest/submitter/submitter_tests.sh
+++ scripts/tests/guest/submitter/submitter_tests.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bats
 
-BASEDIR="$(dirname "$0")"
-case "$BASEDIR" in
-	/*)
-		true
-		;;
-	*)
-		BASEDIR="${PWD}/${BASEDIR}"
-		;;
-esac
-source ../management/submitterSetup.ini
+source ../../../sftp/management/submitterSetup.ini
 
 newuser='first'
 submission='one'
 
 @test "Create submitter 'first'" {
 
-../management/createSubmitter.sh "$newuser"
+../../../sftp/management/createSubmitter.sh "$newuser"
 
 found_user="$(getent passwd "$newuser" | cut -d':' -f1 )"
 [ "$found_user" = "$newuser" ]
@@ -24,7 +15,7 @@ found_user="$(getent passwd "$newuser" | cut -d':' -f1 )"
 
 @test "Create submission '$submission' for all users" {
 
-../management/createSubmission.sh "$submission"
+../../../sftp/management/createSubmission.sh "$submission"
 
 submission_folder_path="${BASE_SUBM_DATA}/${newuser}/${SUBMISSIONS_SUBDIR}/${submission}"
 submission_folder_name="$(basename "$submission_folder_path")"
