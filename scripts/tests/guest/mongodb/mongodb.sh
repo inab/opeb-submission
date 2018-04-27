242e80fe3d545ab37da59e1aa07e4aa31d6e83a9
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-04-27 12:49:23 +0200

    VM #3: Add mongo service

---
+++ scripts/tests/guest/mongodb/mongodb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bats
+
+@test "Check mongodb service running" {
+    [ "$(curl -s localhost:27017)" = "It looks like you are trying to access MongoDB over HTTP on the native driver port." ] && true
+[ $? ]
+}
+
