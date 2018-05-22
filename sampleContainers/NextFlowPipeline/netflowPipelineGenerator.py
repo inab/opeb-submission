5514ea0219d8ddf44b6a2d68f9f4dbe056b39993
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-05-22 12:39:11 +0200

    nextflow: refactor nexflow generator

--- sampleContainers/NextFlowPipeline/netflowPipelineGenerator.py
+++ sampleContainers/NextFlowPipeline/netflowPipelineGenerator.py
@@ -7,15 +7,17 @@ config.read('config.ini')
 
 
 def generate_dockerPreconditions(config):
-    generated_string = 'process dockerPreconditions {\n'
+    generated_string = []
+    generated_string.append('process dockerPreconditions {')
     for section in config.sections():
         for image_name, path in config[section].items():
             if path is None:
-                generated_string += f'\tdocker pull {image_name}\n'
+                generated_string.append(f'\tdocker pull {image_name}')
             else:
-                generated_string += f'\tdocker build -t {image_name} {path}\n'
-    generated_string += '}'
-    return generated_string
+                generated_string.append(
+                    f'\tdocker build -t {image_name} {path}')
+    generated_string.append('}')
+    return '\n'.join(generated_string)
 
 
 print(generate_dockerPreconditions(config))
