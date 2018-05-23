d3c3cc66f4c8d523b24c64ed4ed658dd06c04fc5
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-05-23 09:49:34 +0200

    nextflow: Refactor nextflow config generator

--- sampleContainers/NextFlowPipeline/nextflowPipelineGenerator.py
+++ sampleContainers/NextFlowPipeline/nextflowPipelineGenerator.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
-import configparser
-
-config = configparser.ConfigParser(allow_no_value=True)
-
-config.read('config.ini')
+from configparser import ConfigParser
 
 
-def generate_header(config, data_list):
-    generated_strings = []
-    generated_strings.append('#!/usr/bin/env nextflow')
-    for data in config['InputData']:
-        data_list.append(data)
-        generated_strings.append(f'params.in = "{data}"')
-    return '\n'.join(generated_strings)
+class NextflowConfigGenerator(ConfigParser):
+    def __init__(self, config_file):
+        ConfigParser.__init__(self, allow_no_value=True)
+        self.read(config_file)
+        self.data_list = []
+        self.string_phases = []
+
+    def _generate_header(self):
+        generated_strings = []
+        generated_strings.append('#!/usr/bin/env nextflow')
+        for data in self['InputData']:
+            self.data_list.append(data)
+            generated_strings.append(f'params.in = "{data}"')
+        self.string_phases.append('\n'.join(generated_strings))
+
+    def _generate_dockerPreconditions(self):
+        generated_string = []
+        generated_string.append('process dockerPreconditions {')
+        for section in self.sections():
+            for image_name, path in self[section].items():
+                if path is None:
+                    generated_string.append(f'\tdocker pull {image_name}')
+                else:
+                    generated_string.append(
+                        f'\tdocker build -t {image_name} {path}')
+        generated_string.append('}')
+        self.string_phases.append('\n'.join(generated_string))
+
+    def __str__(self):
+        if not self.string_phases:
+            self._generate_header()
+            self._generate_dockerPreconditions()
+        return '\n'.join(self.string_phases)
 
 
-def generate_dockerPreconditions(config):
-    generated_string = []
-    generated_string.append('process dockerPreconditions {')
-    for section in config.sections():
-        for image_name, path in config[section].items():
-            if path is None:
-                generated_string.append(f'\tdocker pull {image_name}')
-            else:
-                generated_string.append(
-                    f'\tdocker build -t {image_name} {path}')
-    generated_string.append('}')
-    return '\n'.join(generated_string)
-
-
-data_list = []
-string_phases = []
-
-string_phases.append(generate_header(config, data_list))
-string_phases.append(generate_dockerPreconditions(config))
-print('\n'.join(string_phases))
+nextflowConfig = NextflowConfigGenerator('config.ini')
+print(nextflowConfig)
