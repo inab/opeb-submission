a02483b374d49383386687e813549ee890fe55c0
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-05-25 13:13:33 +0200

    NextflowConfigGenerator: Add checkResults method

--- sampleContainers/NextFlowPipeline/nextflowPipelineGenerator.py
+++ sampleContainers/NextFlowPipeline/nextflowPipelineGenerator.py
@@ -49,6 +49,26 @@ class NextflowConfigGenerator(ConfigParser):
         generated_lines.append('}')
         self.string_phases.append('\n'.join(generated_lines))
 
+    def _generate_checkResults(self):
+        generated_lines = []
+        generated_lines.append('process checkResults {')
+        generated_lines.append('')
+        generated_lines.append(self._generate_publishDir())
+        generated_lines.append('')
+        generated_lines.append(self._generate_files(
+            ['docker_image_dependency'] + self.input_data, mode='input'))
+        generated_lines.append('')
+        generated_lines.append(self._generate_files(
+            'canonical_results', mode='output'))
+        generated_lines.append('')
+        generated_lines.append('\t"""')
+        generated_lines.append(
+            f' \tdoValidateAndCopy.sh {" ".join((map(lambda l: "$"+l, self.input_data)))} canonical_results')
+        generated_lines.append('\t"""')
+        generated_lines.append('}')
+
+        self.string_phases.append('\n'.join(generated_lines))
+
     def _generate_publishDir(self):
         return f"\tpublishDir '{self.publishDir}', mode: 'copy', overwrite: true"
 
@@ -67,6 +87,7 @@ class NextflowConfigGenerator(ConfigParser):
         if not self.string_phases:
             self._generate_header()
             self._generate_dockerPreconditions()
+            self._generate_checkResults()
         return '\n'.join(self.string_phases)
 
 
